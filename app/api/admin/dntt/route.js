Savitax-dev/savitax-app

Sửa chính tả "TTPhiDichVu" (V hoa) + rút gọn ô nhập nội dung QR còn 350px
Nội dung QR mặc định trước đó sai "TTPhiDichvu" (v thường), đúng phải
là "TTPhiDichVu". Ô nhập ở panel ĐNTT trước đó giãn hết chiều rộng còn
lại của hàng (flex-1), nay cố định 350px cho gọn.

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>

app/api/admin/dntt/route.js
@@ -62,7 +62,7 @@ export async function GET(request) {
   const monthPad   = String(month).padStart(2, '0')
   const periodCode = client.fee_period === 'quarterly' ? 'Q' + Math.ceil(month / 3) : 'T' + monthPad
   const clientCode = client.client_code || client.tax_code || ''
-  const qrContent  = qrContentParam || (clientCode + '_TTPhiDichvu_' + periodCode + '_Savitax')
+  const qrContent  = qrContentParam || (clientCode + '_TTPhiDichVu_' + periodCode + '_Savitax')
   const bankId     = 'ACB'
   const accountNo  = '3878556868'
   const qrUrl = 'https://img.vietqr.io/image/' + bankId + '-' + accountNo +

components/ClientChecklist.js
@@ -197,7 +197,7 @@ export default function ClientChecklist({ client, clientMonth, onMonthChange, on
       // đồng bộ lại nếu đổi tháng/quý trong lúc panel đang mở (giống hệt b1Label/b1Amount).
       const periodCodeQr = client.fee_period === 'quarterly' ? 'Q' + Math.ceil(clientMonth / 3) : 'T' + String(clientMonth).padStart(2, '0')
       const clientCodeQr = client.client_code || client.tax_code || ''
-      setQrContent(clientCodeQr + '_TTPhiDichvu_' + periodCodeQr + '_Savitax')
+      setQrContent(clientCodeQr + '_TTPhiDichVu_' + periodCodeQr + '_Savitax')
     }
     if (p === 'info') { setEditCred(null); setShowHistory(false); loadCreds() }
     if (p === 'files') { setFileError(''); loadFiles() }
@@ -705,10 +705,10 @@ export default function ClientChecklist({ client, clientMonth, onMonthChange, on
                 className="text-xs px-3 py-1.5 bg-indigo-50 text-indigo-700 border border-indigo-200 rounded-lg hover:bg-indigo-100 disabled:opacity-40 transition-colors font-medium">
                 + Thêm dòng B{extraRows.length+2}
               </button>
-              <div className="flex-1 flex items-center gap-1.5 justify-end min-w-[220px]">
+              <div className="flex items-center gap-1.5 justify-end ml-auto">
                 <label className="text-xs text-gray-400 flex-shrink-0">QR:</label>
                 <input value={qrContent} onChange={e => setQrContent(e.target.value)}
-                  className="flex-1 min-w-0 px-1.5 py-0.5 border border-indigo-200 rounded text-xs font-mono text-indigo-600 text-right focus:outline-none focus:ring-1 focus:ring-indigo-400" />
+                  className="w-[350px] px-1.5 py-0.5 border border-indigo-200 rounded text-xs font-mono text-indigo-600 text-right focus:outline-none focus:ring-1 focus:ring-indigo-400" />
               </div>
             </div>
           </div>

DLTABS/abs-app

app/api/admin/dntt/route.js
@@ -62,7 +62,7 @@ export async function GET(request) {
   const monthPad   = String(month).padStart(2, '0')
   const periodCode = client.fee_period === 'quarterly' ? 'Q' + Math.ceil(month / 3) : 'T' + monthPad
   const clientCode = client.client_code || client.tax_code || ''
-  const qrContent  = qrContentParam || (clientCode + '_TTPhiDichvu_' + periodCode + '_ABS')
+  const qrContent  = qrContentParam || (clientCode + '_TTPhiDichVu_' + periodCode + '_ABS')
   const bankId     = 'VCB'
   const accountNo  = '0351001043923'
   const qrUrl = 'https://img.vietqr.io/image/' + bankId + '-' + accountNo +

components/ClientChecklist.js
@@ -197,7 +197,7 @@ export default function ClientChecklist({ client, clientMonth, onMonthChange, on
       // đồng bộ lại nếu đổi tháng/quý trong lúc panel đang mở (giống hệt b1Label/b1Amount).
       const periodCodeQr = client.fee_period === 'quarterly' ? 'Q' + Math.ceil(clientMonth / 3) : 'T' + String(clientMonth).padStart(2, '0')
       const clientCodeQr = client.client_code || client.tax_code || ''
-      setQrContent(clientCodeQr + '_TTPhiDichvu_' + periodCodeQr + '_ABS')
+      setQrContent(clientCodeQr + '_TTPhiDichVu_' + periodCodeQr + '_ABS')
     }
     if (p === 'info') { setEditCred(null); setShowHistory(false); loadCreds() }
     if (p === 'files') { setFileError(''); loadFiles() }
@@ -706,10 +706,10 @@ export default function ClientChecklist({ client, clientMonth, onMonthChange, on
                 className="text-xs px-3 py-1.5 bg-indigo-50 text-indigo-700 border border-indigo-200 rounded-lg hover:bg-indigo-100 disabled:opacity-40 transition-colors font-medium">
                 + Thêm dòng B{extraRows.length+2}
               </button>
-              <div className="flex-1 flex items-center gap-1.5 justify-end min-w-[220px]">
+              <div className="flex items-center gap-1.5 justify-end ml-auto">
                 <label className="text-xs text-gray-400 flex-shrink-0">QR:</label>
                 <input value={qrContent} onChange={e => setQrContent(e.target.value)}
-                  className="flex-1 min-w-0 px-1.5 py-0.5 border border-indigo-200 rounded text-xs font-mono text-indigo-600 text-right focus:outline-none focus:ring-1 focus:ring-indigo-400" />
+                  className="w-[350px] px-1.5 py-0.5 border border-indigo-200 rounded text-xs font-mono text-indigo-600 text-right focus:outline-none focus:ring-1 focus:ring-indigo-400" />
               </div>
             </div>
           </div>